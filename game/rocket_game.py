fe0b2e6252c125e9330a39853f3e087b4958e144
Sirawich Noipha <pikcolo13@gmail.com>
2024-01-08 21:14:57 +0700

    add module

--- game/rocket_game.py
+++ game/rocket_game.py
@@ -1,10 +1,15 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
 from kivy.graphics import Rectangle
 from kivy.core.window import Window
 from kivy.vector import Vector
 from kivy.clock import Clock
+import random
 
 class Game(FloatLayout):
     pass
