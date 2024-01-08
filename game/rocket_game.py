1544d79f41848b21d04792c5f46936d13f4b905c
Sirawich Noipha <pikcolo13@gmail.com>
2024-01-08 19:45:27 +0700

    add rocket_game.py and rocket_game.kv

--- game/rocket_game.py
+++ game/rocket_game.py
@@ -1,1 +1,17 @@
-import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+class Game(FloatLayout):
+    pass
+
+class RocketApp(App):
+
+    def build(self):
+        return Game()
+
+if __name__ == "__main__":
+    RocketApp().run()
