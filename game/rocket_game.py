3140b48b68183c0a51aac1b5dd1bd67fb880061d
Sirawich Noipha <pikcolo13@gmail.com>
2024-01-11 21:36:25 +0700

    add property size and pos

--- game/rocket_game.py
+++ game/rocket_game.py
@@ -64,6 +64,33 @@ class Entity(object):
         self._instruction = Rectangle(
             pos=self._pos, size=self._size, source=self._source)
         
+    @property
+    def pos(self):
+        return self._pos
+
+    @pos.setter
+    def pos(self, value):
+        self._pos = value
+        self._instruction.pos = self._pos
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+        self._instruction.size = self._size
+
+    @property
+    def source(self):
+        return self._source
+
+    @source.setter
+    def source(self, value):
+        self._source = value
+        self._instruction.source = self._source
+        
 class RocketApp(App):
     def build(self):
         return Game()
